bbb71fa45d626a2269fab8cde33fbd7236abb991
corozco10 <corozco10@mail.csuchico.edu>
2018-03-01 11:54:33 -0800

    Stubbonr Player

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author:Christian Orozco
 #include "SecretDoor/SecretDoor.h"
 #include <iostream>
 
@@ -5,7 +6,20 @@ using namespace std;
 
 int main()
 {
+  int timeswon;
+  int number;
   SecretDoor game;
-    
+  cout<<"How many times do you want to play?\n";
+  cin>>number;
+  for(int i=0;i<number;i++)
+ {
+  game.newGame();
+  game.guessDoorC();
+  game.guessDoorC();
+  if(game.isWinner() == true)
+    timeswon++;
+ }
+  
+  cout<<timeswon<<endl;
   return 0;
 }
